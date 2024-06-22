4caa5eb07ba759d90905abfb758f392672b3a20f
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-22 18:02:53 +0530

    hashing with the mapping

--- hashing/frequency_char.cpp
+++ hashing/frequency_char.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//65-90 A-Z
+//97-122 a-z
 int main()
 {
     string s;
@@ -10,6 +12,8 @@ int main()
 
     // pre compiled
     int hash[26] = {0};
+    // when we have to check for the uppercase and lowercase also then we have to made the hashmap 256
+    //    int hash[256] = {0}; then there are not requirment of the minus with the 'a'
     for (int i = 0; i < s.size(); i++)
     {
         hash[s[i] - 'a'] += 1;
