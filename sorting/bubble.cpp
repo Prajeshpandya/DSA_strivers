67615718114886c13a04eee62c61180be45b0070
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-25 23:36:23 +0530

    bubble sort another approach

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -5,14 +5,29 @@ void bubble(int arr[], int n)
 
 { // after the first iteration the max value at index n-1:
     // so the loop start from
-    for (int i = n - 1; i >= 0; i--)
-    {
-        for (int j = 0; j <= i - 1; j++) // i-1 means the comparision perform between 0th to just before the max index.
-        {
+    // for (int i = n - 1; i >= 0; i--)
+    // {
+    //     // i represents the last index of the current unsorted section of the array.
 
-            if (arr[j] > arr[j + 1])
+    //     for (int j = 0; j <= i - 1; j++) // i-1 means the comparision perform between 0th to just before the max index.
+    //     {
+
+    //         if (arr[j] > arr[j + 1])
+    //         {
+    //             swap(arr[j], arr[j + 1]);
+    //         }
+    //     }
+    // }
+
+    for (int i = 0; i < n; i++)
+    {
+        // i represents the last index of the current unsorted section of the array.
+
+        for (int j = 0; j <n-1-i; j++)        {
+
+            if (arr[j] > arr[j+1])
             {
-                swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j+1]);
             }
         }
     }
